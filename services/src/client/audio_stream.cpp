749941fb340b93a62cf32176cb0576fb2db1ee86
gao-rongxin <gaorongxin1@huawei.com>
2022-01-14 01:20:42 -0800

    fix: 将time_t由_int64回退为long类型，所以回退由_int64所造成打印信息的修改 修改 PRId64为 %ld .
    Signed-off-by: gao-rongxin <gaorongxin1@huawei.com>
    #I4QUJX

--- services/src/client/audio_stream.cpp
+++ services/src/client/audio_stream.cpp
@@ -15,7 +15,6 @@
 
 #include "audio_stream.h"
 #include <vector>
-#include <cinttypes>
 #include "audio_errors.h"
 #include "audio_info.h"
 #include "media_log.h"
@@ -64,7 +63,7 @@ bool AudioStream::GetAudioTime(Timestamp &timestamp, Timestamp::Timestampbase ba
         timestamp.time.tv_sec += (timestamp.time.tv_nsec / TIME_CONVERSION_NS_S);
         timestamp.time.tv_nsec = (timestamp.time.tv_nsec % TIME_CONVERSION_NS_S);
 
-        MEDIA_DEBUG_LOG("AudioStream: GetAudioTime timestamp sec:nsec %{public}" PRId64 ":%{public}ld",
+        MEDIA_DEBUG_LOG("AudioStream: GetAudioTime timestamp sec:nsec %{public}ld:%{public}ld",
                         timestamp.time.tv_sec, timestamp.time.tv_nsec);
         return true;
     }
